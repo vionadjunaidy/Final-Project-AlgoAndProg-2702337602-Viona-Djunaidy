0a8060997e59da59830f9e92bb2d0c5f63afb031
vionadjunaidy <144786402+vionadjunaidy@users.noreply.github.com>
2024-01-08 10:42:17 +0700

    Update admin.py

--- storeSystem/inventory/admin.py
+++ storeSystem/inventory/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import masterProduct
+from .models import masterProduct, Purchase, Sales, Stock
 
-# Register your models here.
+#Allow the models to be created, updated, and deleted through the Django admin page. 
 admin.site.register(masterProduct)
+admin.site.register(Purchase)
+admin.site.register(Sales)
+admin.site.register(Stock)
